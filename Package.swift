ab49f7902abef8bec8d7f9a70a4ae80ccf9f7e37
Sightic <noreply@sightic.com>
2025-04-08 09:02:46 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
         .library(name: "IRISintegrate", targets: ["IRISintegrate"]),
     ],
     targets: [
-        .binaryTarget(name: "IRISintegrate", url: "https://github.com/SighticAnalytics/iris-integrate-ios/releases/download/3.0.0/IRISintegrate.xcframework.zip", checksum: "0f5b88d760c026e56dd235a3c8eb18db529710ef60b0f587ce197e8cc8eee032"),
+        .binaryTarget(name: "IRISintegrate", url: "https://github.com/SighticAnalytics/iris-integrate-ios/releases/download/3.1.0/IRISintegrate.xcframework.zip", checksum: "aa662216fda48f571bea53c559560bdb329dd69ef6f6a7232233d6e0dbed68e4"),
     ]
 )
