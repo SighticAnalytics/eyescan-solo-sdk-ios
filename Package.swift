f98f8c358763a08c83a4216cda4f987198e34efa
Sightic <noreply@sightic.com>
2026-05-12 14:37:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
         .library(name: "EyeScanSoloSDK", targets: ["EyeScanSoloSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "EyeScanSoloSDK", url: "https://github.com/SighticAnalytics/eyescan-solo-sdk-ios/releases/download/3.2.0/EyeScanSoloSDK.xcframework.zip", checksum: "463bf13b0d04fbcf7ea49a1c9a93d5d047bfd9f99dd96ef010bfda302fb048a0"),
+        .binaryTarget(name: "EyeScanSoloSDK", url: "https://github.com/SighticAnalytics/eyescan-solo-sdk-ios/releases/download/3.3.0/EyeScanSoloSDK.xcframework.zip", checksum: "846eef979ce6d53762a35879c90a7cb32e9c654f0e2ca1c552a7df2cacbc8830"),
     ]
 )
